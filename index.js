c6d77bcb3c75664dece07559b97fe0d9b5bb6487
maabit01 <maabit01@hs-esslingen.de>
2022-04-23 22:17:20 +0200

    cookies

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import {config} from "dotenv";
 import path from "path";
 import mysql from "mysql";
 import {v4} from "uuid";
+import cookieParser from "cookie-parser";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -23,19 +24,11 @@ app.set("view engine", "ejs");
 app.set("views", path.resolve(__dirname, "views"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+app.use(cookieParser());
 
 let connection = mysql.createPool({
     host: MYSQL_HOST, port: MYSQL_PORT, user: MYSQL_USER, password: MYSQL_PASSWORD, database: MYSQL_DATABSE
 });
-
-// app.get("/", (req, res) => {
-//     const username = req.query.username;
-//     const password = req.query.password;
-//     connection.query("SELECT username,password FROM user WHERE username =" + "'" + username + "'" + " AND password =" + "'" + password + "'", (err, rows, fields) => {
-//         if (err) throw err;
-//         res.send(rows);
-//     });
-// });
 
 async function getPostsWithComments() {
     return new Promise(async (resolve, reject) => {
@@ -73,23 +66,20 @@ async function getPasswordByUsername(username) {
 }
 
 app.get("/", async (req, res) => {
-    console.log("sessions", sessions);
     let username = "";
     if (req.cookies) {
         const sessionToken = req.cookies["session_token"];
-        console.log("sessionToken", sessionToken);
         const userSession = sessions[sessionToken];
-        console.log("userSession", userSession);
-        username = userSession.username;
-        console.log("username", username);
+        if (userSession != undefined) {
+            username = userSession.username;
+        }
     }
     await getPostsWithComments().then((posts) => {
-        res.render("pages/index", {posts: posts, username: ""});
+        res.render("pages/index", {posts: posts, username: username});
     });
 });
 
 app.get("/post", (req, res) => {
-
     connection.query("SELECT p.postID, p.content, p.post_userID, u.username FROM itseclab.post AS p JOIN itseclab.user AS u ON p.post_userID=u.userID; ", (err, rows, fields) => {
         if (err) throw err;
         // console.log("/posts...", rows);
@@ -98,20 +88,42 @@ app.get("/post", (req, res) => {
 });
 
 app.post("/post/new", (req, res) => {
-    if (req.cookies) {
-        const sessionToken = req.cookies["session_token"];
-        console.log("sessionToken", sessionToken);
-        const userSession = sessions[sessionToken];
-        console.log("userSession", userSession);
-        let username = userSession.username;
-        console.log("username", username);
+
+    if (!req.cookies) {
+        res.status(401).end();
+        return;
     }
+
+    // We can obtain the session token from the requests cookies, which come with every request
+    const sessionToken = req.cookies["session_token"];
+    if (!sessionToken) {
+        // If the cookie is not set, return an unauthorized status
+        res.status(401).end();
+        return;
+    }
+
+    // We then get the session of the user from our session map
+    // that we set in the signinHandler
+    let userSession = sessions[sessionToken];
+    if (!userSession) {
+        // If the session token is not present in session map, return an unauthorized error
+        res.status(401).end();
+        return;
+    }
+    // if the session has expired, return an unauthorized error, and delete the
+    // session from our map
+    if (userSession.isExpired()) {
+        delete sessions[sessionToken];
+        res.status(401).end();
+        return;
+    }
+
     const content = req.body.content;
-    //username is saved in cookie
-    const username = "mattias";
+    const username = userSession.username;
     connection.query("INSERT INTO itseclab.post (content, post_userID)\n VALUES (?, (SELECT userID FROM user WHERE username LIKE ?));", [content, username], (err, rows, fields) => {
         if (err) throw err;
-        res.send("New post created.");
+        res.redirect("/");
+        res.status(200).end();
     });
 });
 
@@ -125,14 +137,42 @@ app.get("/comment", (req, res) => {
 });
 
 app.post("/comment/new", (req, res) => {
+    if (!req.cookies) {
+        res.status(401).end();
+        return;
+    }
+
+    // We can obtain the session token from the requests cookies, which come with every request
+    const sessionToken = req.cookies["session_token"];
+    if (!sessionToken) {
+        // If the cookie is not set, return an unauthorized status
+        res.status(401).end();
+        return;
+    }
+
+    // We then get the session of the user from our session map
+    // that we set in the signinHandler
+    let userSession = sessions[sessionToken];
+    if (!userSession) {
+        // If the session token is not present in session map, return an unauthorized error
+        res.status(401).end();
+        return;
+    }
+    // if the session has expired, return an unauthorized error, and delete the
+    // session from our map
+    if (userSession.isExpired()) {
+        delete sessions[sessionToken];
+        res.status(401).end();
+        return;
+    }
+
     const comment_postID = req.body.comment_postID;
     const content = req.body.content;
-    console.log("req.body", req.body);
-    //username is saved in cookie
-    const username = "mattias";
+    const username = userSession.username;
     connection.query("INSERT INTO itseclab.comment(content, comment_userID, comment_postID) VALUES (?, (SELECT userID from user WHERE username = ?), ?);", [content, username, comment_postID], (err, rows, fields) => {
         if (err) throw err;
-        res.send("root");
+        res.redirect("/");
+        res.status(200).end();
     });
 });
 
@@ -152,17 +192,16 @@ class Session {
 // this object stores the users sessions. For larger scale applications, you can use a database or cache for this purpose
 const sessions = {};
 
-app.get("/login", (req, res) => {
-    // const username = req.body.username;
-    // const password = req.body.password;
-    const username = "mattias";
-    const password = "mattias";
+app.post("/login", (req, res) => {
+    const username = req.body.username;
+    const password = req.body.password;
 
     if (!username) {
         // If the username isn't present, return an HTTP unauthorized code
         res.status(401).end();
         return;
     }
+
     getPasswordByUsername(username).then((result) => {
         let expectedPassword = JSON.parse(JSON.stringify(result))[0].password;
         if (!expectedPassword || expectedPassword !== password) {
@@ -173,14 +212,41 @@ app.get("/login", (req, res) => {
         const sessionToken = v4();
         console.log(sessionToken);
         const now = new Date();
-        const expiresAt = new Date(+now * (60 * 60 * 24 * 7) * 1000);
+        const expiresAt = new Date(+now + (60 * 60 * 24 * 7) * 1000);
         const session = new Session(username, expiresAt);
         sessions[sessionToken] = session;
         console.log("sessions", sessions);
         res.cookie("session_token", sessionToken, {expires: expiresAt});
+        res.redirect("/");
         res.end();
     });
 });
+
+function checkCookie(cookie) {
+    // We can obtain the session token from the requests cookies, which come with every request
+    const sessionToken = cookie["session_token"];
+    if (!sessionToken) {
+        // If the cookie is not set, return an unauthorized status
+        res.status(401).end();
+        return;
+    }
+
+    // We then get the session of the user from our session map
+    // that we set in the signinHandler
+    let userSession = sessions[sessionToken];
+    if (!userSession) {
+        // If the session token is not present in session map, return an unauthorized error
+        res.status(401).end();
+        return;
+    }
+    // if the session has expired, return an unauthorized error, and delete the
+    // session from our map
+    if (userSession.isExpired()) {
+        delete sessions[sessionToken];
+        res.status(401).end();
+        return;
+    }
+}
 
 let server = app.listen(EXPRESS_PORT, EXPRESS_HOST, () => {
     console.log(`App listening on ${EXPRESS_HOST}:${EXPRESS_PORT}`);
